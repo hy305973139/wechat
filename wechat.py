fa5ba95da0638ddc4f01d3ec7c16240a457a1c78
hy305973139 <30432439+hy305973139@users.noreply.github.com>
2018-09-27 09:56:01 +0800

    Update wechat.py

--- wechat.py
+++ wechat.py
@@ -18,27 +18,7 @@ def get_response(msg):
 @itchat.msg_register([itchat.content.TEXT], isGroupChat=True)
 def print_content(msg):
     defaultReply = 'I received: ' + msg['Text']
-    num= random.randint(1,10)
-    print(num)
-    if num==1:
-        x='老子跟你说哦，'
-    elif num==2:
-        x='妈了个逼,'
-    elif num==3:
-        x= '傻逼我跟你说，'
-    elif num==4:
-        x='儿砸，'
-    elif num== 5:
-        x='爸爸告诉你，'
-    elif num== 6:
-        x='叮叮叮，咚咚咚，登登等登瞪瞪,'
-    elif num== 7:
-        x='操你妈,'
-    elif num==8:
-        x='妈了个逼，'
-    else:
-        x= '你是傻逼吗,'
-    reply = x + get_response(msg['Text'])
+    reply = get_response(msg['Text'])
     return reply or defaultReply
 itchat.auto_login(enableCmdQR=True)
 itchat.run()
